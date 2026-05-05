e4d69108408f3043a0628bf49b942c72409a7beb
murillonb <murillo_099@hotmail.com>
2026-05-05 17:38:16 +0200

    added counter and filter

--- js/bd.js
+++ js/bd.js
@@ -1,25 +1,39 @@
 import formatDate from './format-date.js';
+import Score from './score.js';
+
+const score = new Score();
 
 export const bd = {
   tasks: JSON.parse(localStorage.getItem('tasks')) || [],
 
-  addTask(title) {
-    if (!title.trim()) return;
+  addTask(task) {
+    if (!task.trim()) return;
 
     this.tasks.push({
       id: new Date().getTime(),
-      task: title.trim(),
+      task: task.trim(),
       date: formatDate(new Date()),
       status: 'open',
     });
 
-    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.updateLocalStorage();
   },
 
   removeTask(idTask) {
     const index = this.tasks.findIndex(({ id }) => id === idTask);
     this.tasks.splice(index, 1);
+    this.updateLocalStorage();
+  },
 
+  changeStatus(idTask) {
+    const index = this.tasks.findIndex(({ id }) => id === idTask);
+    this.tasks[index].status =
+      this.tasks[index].status === 'open' ? 'done' : 'open';
+    this.updateLocalStorage();
+  },
+
+  updateLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    score.countTasks();
   },
 };
--- js/task.js
+++ js/task.js
@@ -6,39 +6,67 @@ export default class Task {
     this.taskList = document.querySelector('[data-task-list]');
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.excluirTask = this.excluirTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.changeStatus = this.changeStatus.bind(this);
+  }
+
+  changeStatus(e) {
+    const id = e.target.getAttribute('data-check');
+    bd.changeStatus(Number(id));
+    this.loadTasks();
+  }
+
+  addCheckedEvent() {
+    this.checkboxs = document.querySelectorAll('[data-check]');
+    this.checkboxs.forEach((box) =>
+      box.addEventListener('change', this.changeStatus),
+    );
+  }
+
+  deleteTask({ target }) {
+    const id = target.getAttribute('data-id');
+    bd.removeTask(Number(id));
+    this.loadTasks();
+  }
+
+  addDeleteButtonEvent() {
+    this.buttons = document.querySelectorAll('[data-btn-exc]');
+    this.buttons.forEach((btn) =>
+      btn.addEventListener('click', this.deleteTask),
+    );
   }
 
   createTask(task) {
     const li = document.createElement('li');
     li.classList.add('task', 'box');
     li.innerHTML = `
-      <label class="font-task">
-        <input type="checkbox" name="status" id="status">
-        <span>${task.task}</span>
-      </label>
-      <div>
-        <span class="date">${task.date}</span>
-        <button class="btn" data-btn-exc data-id="${task.id}">Excluir</button>
-      </div>`;
+    <label class="font-task">
+      <input type="checkbox" name="status" id="status" ${task.status === 'done' ? 'checked' : ''} data-check="${task.id}">
+      <span>${task.task}</span>
+    </label>
+    <div>
+      <span class="date">${task.date}</span>
+      <button class="btn" data-btn-exc data-id="${task.id}">Excluir</button>
+    </div>`;
 
     return li;
   }
 
   loadTasks() {
     this.taskList.innerHTML = '';
-
-    bd.tasks.forEach((task) =>
-      this.taskList.appendChild(this.createTask(task)),
-    );
+    const filter = localStorage.getItem('filter');
+    bd.tasks.forEach((task) => {
+      if (filter === 'All') {
+        this.taskList.appendChild(this.createTask(task));
+      } else if (filter === 'Done' && task.status === 'done') {
+        this.taskList.appendChild(this.createTask(task));
+      } else if (filter === 'Open' && task.status === 'open') {
+        this.taskList.appendChild(this.createTask(task));
+      }
+    });
 
     this.addDeleteButtonEvent();
-  }
-
-  excluirTask({ target }) {
-    const id = target.getAttribute('data-id');
-    bd.removeTask(Number(id));
-    this.loadTasks();
+    this.addCheckedEvent();
   }
 
   handleSubmit(e) {
@@ -46,13 +74,6 @@ export default class Task {
     bd.addTask(this.form.task.value);
     this.loadTasks();
     this.form.reset();
-  }
-
-  addDeleteButtonEvent() {
-    this.buttons = document.querySelectorAll('[data-btn-exc]');
-    this.buttons.forEach((btn) =>
-      btn.addEventListener('click', this.excluirTask),
-    );
   }
 
   addSubmitEvent() {
